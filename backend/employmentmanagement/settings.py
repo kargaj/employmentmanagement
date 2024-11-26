eb521096599bf8d883658ef84d4600f14962aa70
Karolina Lichoń <gajewskakarolina8@gmail.com>
2024-11-26 18:47:49 +0100

    Add docker-compose setup for backend. Change database to postgresql

--- backend/employmentmanagement/settings.py
+++ backend/employmentmanagement/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,7 +26,7 @@ SECRET_KEY = 'django-insecure-%ggzy$cj2jns)=!vyke%2*jf9o-##++wkkd24v3*8l_&hw(^dv
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -77,8 +78,12 @@ WSGI_APPLICATION = 'employmentmanagement.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv('POSTGRES_DB', 'your_db'),
+        'USER': os.getenv('POSTGRES_USER', 'your_user'),
+        'PASSWORD': os.getenv('POSTGRES_PASSWORD', 'your_password'),
+        'HOST': os.getenv('POSTGRES_HOST', 'localhost'),
+        'PORT': os.getenv('POSTGRES_PORT', '5432'),
     }
 }
 
