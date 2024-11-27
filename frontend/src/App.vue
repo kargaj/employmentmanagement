ba11ab59af7a93e8bc24c25f3e5807eda3c0d661
Karolina Lichoń <gajewskakarolina8@gmail.com>
2024-11-27 20:07:31 +0100

    Add view: List of employers

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -8,5 +8,4 @@
 </template>
 
 <script setup>
-  //
 </script>
--- frontend/src/components/EmployersListView.vue
+++ frontend/src/components/EmployersListView.vue
@@ -1,8 +1,66 @@
-<script setup>
+<template>
+  <v-container>
+    <v-data-table
+      :headers="headers"
+      :items="employees"
+      item-value="name"
+      class="elevation-1"
+      hide-default-footer>
+      <template v-slot:top>
+        <v-toolbar flat>
+          <v-toolbar-title>List of employers</v-toolbar-title>
+        </v-toolbar>
+      </template>
+
+      <template v-slot:item.name="{ item }">
+        <td :key="item.id">{{ item.name }}</td>
+      </template>
+      <template v-slot:item.age="{ item }">
+        <td >{{ item.age }}</td>
+      </template>
+      <template v-slot:item.department="{ item }">
+        <td>{{ item.department }}</td>
+      </template>
+    </v-data-table>
+  </v-container>
+</template>
+
+<script>
+import { EMPLOYERS_API } from "../constants/api";
+
+import { ref, onMounted } from 'vue';
+import axios from 'axios';
+
+
+export default {
+  setup() {
+    const headers = [
+      { text: 'Name', align: 'start', value: 'name' },
+      { text: 'Department', value: 'department' },
+      { text: 'Age', align: 'end', value: 'age' },
+    ];
+
+    const employers = ref(undefined);
+
+    onMounted(async () => {
+      try {
+        const response = await axios.get(EMPLOYERS_API);
+        employers.value = response.data;
+      } catch (error) {
+        console.error('Error fetching employee data:', error);
+      }
+    });
+
+    return {
+      headers,
+      employees: employers
+    };
+  }
+};
 </script>
 
-<template>
-  <main>
-    List
-  </main>
-</template>
+<style scoped lang="scss">
+  .v-data-table {
+    max-width: 100%;
+  }
+</style>
